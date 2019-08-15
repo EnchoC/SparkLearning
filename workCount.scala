9a59a8fb80733c470459d2d2fc6d35bc15a5d809
Encho <encho@EnchodeMacBook-Pro.local>
2019-08-15 10:50:09 +0800

    update

--- workCount.scala
+++
@@ -1,4 +0,0 @@
-val textData = sc.textFile('./workCountData.txt')
-val stringRdd = textData.flatMap(t => t.split(' '))
-val countsRdd = stringRdd.map(word => (word, 1)).reduceByKey(_ + _)
-countsRdd.saveAsTextFile('./output')
